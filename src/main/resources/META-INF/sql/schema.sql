53632409ea0a82fbc89a6276a7affc3f0d511bd1
José Ramón Balsas <jrbalsas@ujaen.es>
2024-02-25 12:51:48 +0100

    encrypted password for database sample users

--- src/main/resources/META-INF/sql/schema.sql
+++ src/main/resources/META-INF/sql/schema.sql
@@ -1,7 +1,7 @@
--- Manual tables definition, e.g. non JPA managed tables (IMPORTANT: only ONE ROW per sentence)
+-- Manual tables definition, i.e. non JPA managed tables (IMPORTANT: only ONE ROW per sentence)
 
 --Delete previous tables
-DROP TABLE authinfo;
+-- DROP TABLE authinfo;
 
-create table if not exists authinfo ( id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY, dni VARCHAR(10), password VARCHAR(512), rolname VARCHAR(50) );
+-- create table if not exists authinfo ( id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY, dni VARCHAR(10), password VARCHAR(512), rolname VARCHAR(50) );
 
