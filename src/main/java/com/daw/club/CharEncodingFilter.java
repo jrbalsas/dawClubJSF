f674880feafff9dcf28f93e37db8252b47084ab9
José Ramón Balsas <jrbalsas@ujaen.es>
2025-03-05 19:27:21 +0100

    Added optional UTF-8 encoding filter
    Required for working with UTF-8 chars in forms

---
+++ src/main/java/com/daw/club/CharEncodingFilter.java
@@ -0,0 +1,14 @@
+package com.daw.club;
+
+/* Optional filter to force UTF-8 characters encoding on form submissions */
+
+//@WebFilter("/*")
+//public class CharEncodingFilter implements Filter {
+//
+//        @Override
+//        public void doFilter(ServletRequest request, ServletResponse response,
+//                             FilterChain chain) throws ServletException, IOException {
+//            request.setCharacterEncoding("UTF-8");
+//            chain.doFilter(request, response);
+//        }
+//}
